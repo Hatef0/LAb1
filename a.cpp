dbdbcdf78428d122df5ec343cfabf57055e7c6f8
Hatef0 <hatefochalak@gmail.com>
2025-06-05 01:26:05 +0500

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cout << "Whats your name? ";
+    cin >> name;
+
+    cout << "Hello, " << name << " ! " << endl;
+
+    return 0;
+
+}
